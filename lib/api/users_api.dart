983765a31a0cdf0d8c740a2e58d85d6b0c48415b
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-02-03 13:32:20 -0300

    feat: add endpoint

---
+++ lib/api/users_api.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_getx_esp_contador/models/users.dart';
+
+class UsersAPI {
+  UsersAPI._internal();
+  static UsersAPI _instance = UsersAPI._internal();
+  static UsersAPI get instance => _instance;
+
+  final Dio _dio = Dio();
+
+  Future<List<User>> getUsers(int page) async {
+    try {
+      final Response response = await this
+          ._dio
+          .get('https://reqres.in/api/users', queryParameters: {"page": page});
+
+      return (response.data['data'] as List)
+          .map((users) => User.fromJson(users))
+          .toList();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
